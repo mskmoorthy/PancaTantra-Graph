655782eaa51b539ceb7bb9d71bed7c16f37b95d7
T.V Raman <tv.raman.tv@gmail.com>
2022-03-12 14:51:22 -0800

    classes

--- src/panch/classes.py
+++ src/panch/classes.py
@@ -1,19 +1,41 @@
-"""
-Classes For Panchatantra  Graphs
-"""
+"""Classes For Panchatantra  Graphs"""
 
 
 class Story:
     """ A Story consists of Characters, etc."""
+    id  # unique id (computed)
     title = ""
     stories = []
     narrator = ""
     moral = ""
 
+    def __init__(self,
+                 title="",
+                 stories=[],
+                 narrator="",
+                 moral=""):
+        "Initialize a Story"
+        self.title = title
+        self.stories = stories
+        self.narrator = narrator
+        self.moral = moral
+
 
 class Character:
     """A character in a Story"""
+    id  # Unique id (computed)
     name = ""
     said = []
     narrated = []
     appears_in = []
+
+    def __init__(self,
+                 name="",
+                 said=[],
+                 narrated=[],
+                 appears_in=[]):
+        "Initialize a Character"
+        self.name = name
+        self.narrated = narrated
+        self.said = said
+        self.appears_in = appears_in
