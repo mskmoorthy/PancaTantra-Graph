21590a3300a9da15641b3f56ee12d1e9d2e636bc
T.V Raman <tv.raman.tv@gmail.com>
2022-03-13 19:24:12 -0700

    entities

---
+++ src/panch/entities.py
@@ -0,0 +1,9 @@
+"""
+Instances that make up the Panchatantra: Story, Character
+"""
+
+# cast: Holds all known Character instances:
+cast = []
+
+# stories: Holds all known Story instances:
+stories = []
