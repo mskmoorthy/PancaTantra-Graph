d3a247038ca4800855a617490722fdb4f583c9bc
T.V Raman <tv.raman.tv@gmail.com>
2022-03-15 07:24:53 -0700

    cast

--- src/panch/entities.py
+++ src/panch/entities.py
@@ -5,18 +5,14 @@ from classes import Story, Character
 from pprint import pprint
 import csv
 
-instances = []
+cast = {}
 reader = csv.DictReader(open('cast.csv'), skipinitialspace=True)
-for row in reader:
-    instances.append(Character(**row))
+cast = {row['name']: Character(**row) for row in reader}
 
-# cast: Holds all known  instances indexed by name
-
-cast = {c.name: c for c in instances}
 # stories: Holds all known Story instances:
 stories = {}
 stories['book-1'] = Story(title='book-1', cast=cast)
 if __name__ == '__main__':
     print("Story:")
-
-    [pprint(vars(c)) for c in cast]
+    pprint(vars(stories['book-1']))
+    [pprint(vars(v)) for v in cast.values()]
