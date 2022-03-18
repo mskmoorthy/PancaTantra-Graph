1bd2b19900323ddafd19df0e8cbf90962910b9c8
T.V Raman <tv.raman.tv@gmail.com>
2022-03-18 09:45:45 -0700

    initial show method

--- src/panch/classes.py
+++ src/panch/classes.py
@@ -22,18 +22,40 @@ class Story(DictInit):
         'title', 'told_by', 'told_to', 'moral', 'url', 'cast', 'stories'
     ]
 
+    def show(self):
+        "Print out instance."
+        for f in self._fields:
+            try:
+                getattr(self, f)
+                pprint(f)
+                if (f == 'cast'):
+                    [c.show() for c in getattr(self, f)]
+                else:
+                    pprint(getattr(self, f))
+            except AttributeError:
+                pass
+
 
 class Character(DictInit):
     """Character(name = ..., ...) A story character"""
     _fields = ['name', 'species', 'nature', 'color', 'narrated']
 
+    def show(self):
+        "Print out instance."
+        for f in self._fields:
+            try:
+                getattr(self, f)
+                pprint(f)
+                pprint(getattr(self, f))
+            except AttributeError:
+                pass
+
 
 if __name__ == '__main__':
     c = Character(name='rusty', species='lion', nature='brave', color='red')
-    pprint(vars(c))
     s = Story(title="Lion, Bull and Two Jackals",
               moral='friendship and villainy ',
               told_by='panchatantra',
               cast=[c],
               stories=[])
-    pprint(vars(s))
+    s.show()
--- src/panch/entities.py
+++ src/panch/entities.py
@@ -3,17 +3,16 @@ Instances that make up the Panchatantra: Story, Character
 """
 
 from classes import Story, Character
-from pprint import pprint
 import csv
+from pprint import pprint
 
 reader = csv.DictReader(open('cast.csv'), skipinitialspace=True)
 cast = {row['name']: Character(**row) for row in reader}
 
 # stories: Holds all known Story instances:
 stories = {}
-stories['book-1'] = Story(title='book-1', cast=cast)
+stories['book-1'] = Story(title='book-1', cast=[cast[c] for c in cast])
 
 if __name__ == '__main__':
     print("Story:")
-    pprint(vars(stories['book-1']))
-    [pprint(vars(v)) for v in cast.values()]
+    stories['book-1'].show()
