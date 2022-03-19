3efb24c9eac11927340803d19f2daa5f411fc937
T.V Raman <tv.raman.tv@gmail.com>
2022-03-18 19:37:41 -0700

    read stories from book-1.csv

--- src/panch/entities.py
+++ src/panch/entities.py
@@ -9,8 +9,8 @@ reader = csv.DictReader(open('cast.csv'), skipinitialspace=True)
 cast = {row['name']: Character(**row) for row in reader}
 
 # stories: Holds all known Story instances:
-stories = {}
-stories['book-1'] = Story(title='book-1', cast=[cast[c] for c in cast])
+reader = csv.DictReader(open('book-1.csv'), skipinitialspace=True)
+stories = {row['title']: Story(**row) for row in reader}
 
 if __name__ == '__main__':
-    stories['book-1'].show()
+    stories["wedge-pulling monkey"].show()
