82bff01d831b990e469dc602e90defd76bda4494
T.V Raman <tv.raman.tv@gmail.com>
2022-03-18 18:52:54 -0700

    cleanup

--- src/panch/entities.py
+++ src/panch/entities.py
@@ -4,7 +4,6 @@ Instances that make up the Panchatantra: Story, Character
 
 from classes import Story, Character
 import csv
-from pprint import pprint
 
 reader = csv.DictReader(open('cast.csv'), skipinitialspace=True)
 cast = {row['name']: Character(**row) for row in reader}
@@ -14,5 +13,4 @@ stories = {}
 stories['book-1'] = Story(title='book-1', cast=[cast[c] for c in cast])
 
 if __name__ == '__main__':
-    print("Story:")
     stories['book-1'].show()
