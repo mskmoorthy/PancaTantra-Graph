25f1d399aaf20f83fb33b956f7ef3b8e1c5ba869
T.V Raman <tv.raman.tv@gmail.com>
2022-03-19 13:32:44 -0700

    updated cast

--- src/panch/scratch.py
+++ src/panch/scratch.py
@@ -2,9 +2,13 @@
 
 from entities import cast, stories
 
-# Missing characters in cast:
 
-c = [stories[id].told_to
-     for id in stories] + [stories[id].told_by for id in stories]
-new = [w for w in c if w not in cast]
-u = [i for i in {k: 1 for k in new}]
+# Missing characters in cast:
+def cast_missing():
+    c = [stories[id].told_to
+         for id in stories] + [stories[id].told_by for id in stories]
+    new = [w for w in c if w not in cast]
+    return [i for i in {k: 1 for k in new}]
+
+
+print(cast_missing())
