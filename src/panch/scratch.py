3f060d9ab844b3965ec66caf447b539c57d045e4
T.V Raman <tv.raman.tv@gmail.com>
2022-03-19 18:32:10 -0700

    cleanup

--- src/panch/scratch.py
+++ src/panch/scratch.py
@@ -1,14 +1,13 @@
 # Scratchpad for trying out short code snippets
-import importlib
 import entities
 
 
 # Missing characters in cast:
 def cast_missing():
-    c = [entities.stories[id].told_to for id in entities.stories
-         ] + [entities.stories[id].told_by for id in entities.stories]
-    new = [w for w in c if w not in entities.cast]
-    return [i for i in {k: 1 for k in new}]
+    s = entities.stories
+    c = [s[id].told_to for id in s] + [s[id].told_by for id in s]
+    unseen = [w for w in c if w not in entities.cast]
+    return [i for i in {k: 1 for k in unseen}]
 
 
 print(cast_missing())
