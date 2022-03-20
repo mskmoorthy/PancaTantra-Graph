128b01d1a3a100ee9eb3f663f9c093f6401e0abc
T.V Raman <tv.raman.tv@gmail.com>
2022-03-19 19:25:16 -0700

    use set

--- src/panch/scratch.py
+++ src/panch/scratch.py
@@ -6,8 +6,8 @@ import entities
 def cast_missing():
     s = entities.stories
     c = [s[id].told_to for id in s] + [s[id].told_by for id in s]
-    unseen = [w for w in c if w not in entities.cast]
-    return [i for i in {k: 1 for k in unseen}]
+    unseen = {w for w in c if w not in entities.cast}
+    return [i for i in unseen]
 
 
 print(cast_missing())
