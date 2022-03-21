042223886399c6e00220295e07fde850446632a7
T.V Raman <tv.raman.tv@gmail.com>
2022-03-21 16:56:20 -0700

    v1

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -5,20 +5,20 @@ __author__ = "tv.raman.tv@gmail.com"
 
 import pygraphviz as pgv
 import entities
-
+# aliases
 b = entities.book_1
 c = entities.cast_1
 
-g = pgv.AGraph()
+g = pgv.AGraph(directed=True, layout="circo")
 g.add_nodes_from([name for name in c])
-g.add_nodes_from([b[i].title for i in b])
-g.add_nodes_from([b[i].moral for i in b])
-
 for i in b:
     g.add_edge(b[i].told_by, b[i].title)
     g.add_edge(b[i].title, b[i].told_to)
     g.add_edge(b[i].moral, b[i].title)
+    if b[i].stories is not None:
+        [g.add_edge(b[i].title, b[j].title) for j in b[i].stories]
+
 g.layout(prog="circo")  # layout with default (neato)
 g.write("book-1.dot")
 
-#g.draw("book-1.png")
+g.draw("book-1.png")
