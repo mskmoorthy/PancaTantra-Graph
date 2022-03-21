a73e0d3b915ffd04d1f1ea180ceebde510f634c9
T.V Raman <tv.raman.tv@gmail.com>
2022-03-21 16:46:04 -0700

    runs!

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -6,15 +6,19 @@ __author__ = "tv.raman.tv@gmail.com"
 import pygraphviz as pgv
 import entities
 
-A = pgv.AGraph()
+b = entities.book_1
+c = entities.cast_1
 
-A.add_edge(1, 2)
-A.add_edge(2, 3)
-A.add_edge(1, 3)
+g = pgv.AGraph()
+g.add_nodes_from([name for name in c])
+g.add_nodes_from([b[i].title for i in b])
+g.add_nodes_from([b[i].moral for i in b])
 
-print(A.string())  # print to screen
-A.write("simple.dot")  # write to simple.dot
+for i in b:
+    g.add_edge(b[i].told_by, b[i].title)
+    g.add_edge(b[i].title, b[i].told_to)
+    g.add_edge(b[i].moral, b[i].title)
+g.layout(prog="circo")  # layout with default (neato)
+g.write("book-1.dot")
 
-B = pgv.AGraph("simple.dot")  # create a new graph from file
-B.layout()  # layout with default (neato)
-B.draw("simple.png")  # draw png
+#g.draw("book-1.png")
