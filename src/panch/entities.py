a7c8a5b5a86be7a6c66352fb6005facf9ca7ae1d
T.V Raman <tv.raman.tv@gmail.com>
2022-03-21 07:37:40 -0700

    stories_finalize

--- src/panch/entities.py
+++ src/panch/entities.py
@@ -5,12 +5,22 @@ __author__ = "tv.raman.tv@gmail.com"
 from classes import Story, Character
 import csv
 
+
+def stories_finalize(table):
+    "Post process stories in table as needed."
+    for id in table:
+        s = table[id]
+        if (isinstance(s.stories, str)):
+            s.stories = s.stories.split(",")
+
+
 reader = csv.DictReader(open('cast.csv'), skipinitialspace=True)
 cast = {row['name']: Character(**row) for row in reader}
 
 # stories: Holds all known Story instances:
 reader = csv.DictReader(open('book-1.csv'), skipinitialspace=True)
 stories = {row['index']: Story(**row) for row in reader}
+stories_finalize(stories)
 
 if __name__ == '__main__':
     [stories[s].show() for s in stories]
