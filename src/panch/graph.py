e3ee8afa1a459c1f733cbde130669197346164f6
T.V Raman <tv.raman.tv@gmail.com>
2022-03-21 17:13:38 -0700

    g->graph for readability

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -9,16 +9,16 @@ import entities
 b = entities.book_1
 c = entities.cast_1
 
-g = pgv.AGraph(directed=True, layout="circo", name="Panchatantra Book 1")
-g.add_nodes_from([name for name in c])
+graph = pgv.AGraph(directed=True, layout="circo", name="Panchatantra Book 1")
+graph.add_nodes_from([name for name in c])
 for i in b:
-    g.add_edge(b[i].told_by, b[i].title)
-    g.add_edge(b[i].title, b[i].told_to)
-    g.add_edge(b[i].moral, b[i].title)
+    graph.add_edge(b[i].told_by, b[i].title)
+    graph.add_edge(b[i].title, b[i].told_to)
+    graph.add_edge(b[i].moral, b[i].title)
     if b[i].stories is not None:
-        [g.add_edge(b[i].title, b[j].title) for j in b[i].stories]
+        [graph.add_edge(b[i].title, b[j].title) for j in b[i].stories]
 
-g.layout(prog="circo")  # layout with default (neato)
-g.write("book-1.dot")
+graph.layout(prog="circo")  # layout with default (neato)
+graph.write("book-1.dot")
 
-#g.draw("book-1.png")
+#graph.draw("book-1.png")
