348cad9f6a60c65d8148a7db09f170abf636dd18
T.V Raman <tv.raman.tv@gmail.com>
2022-03-22 17:13:53 -0700

    data: fix redundant edges to nested stories in book-1.csv, add shape=box

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -14,6 +14,7 @@ graph = pgv.AGraph(directed=True,
                    size="8,8",
                    label="graphatantra")
 [graph.add_node(c[i].name, color=c[i].color, style="filled") for i in c]  #Cast
+graph.node_attr.update(color="SkyBlue", shape="box")
 for i in b:  #stories
     graph.add_edge(b[i].told_by, b[i].title)
     graph.add_edge(b[i].title, b[i].told_to)
