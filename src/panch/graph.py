a4c12267da1f40e6c027b69b7e7b9cc6dde6e618
T.V Raman <tv.raman.tv@gmail.com>
2022-03-26 14:42:10 -0700

    subgraph

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -30,6 +30,8 @@ for i in b:
 animals = ['rusty', 'lively', 'crafty', 'cautious']
 # Rank explained: https://www.worthe-it.co.za/blog/2017-09-19-quick-introduction-to-graphviz.html#:~:text=Ranks%20and%20Subgraphs,placed%20further%20to%20the%20right.
 graph.add_subgraph(animals, rank="same", name="main")
+top = [b['34'].title, b['34'].told_by, b['34'].told_to]
+graph.add_subgraph(top, rank="same", name="outer")
 graph.unflatten("-f -l3 -c 5").layout()
 graph.write("book-1.dot")
 graph.draw("book-1.pdf")
