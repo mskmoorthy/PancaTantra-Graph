5b2599f96409f9e48bb30d4c9eb3425f65374b51
T.V Raman <tv.raman.tv@gmail.com>
2022-03-26 14:48:44 -0700

    cleanup

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -28,10 +28,10 @@ for i in b:
         ]  # contained stories
 
 animals = ['rusty', 'lively', 'crafty', 'cautious']
-# Rank explained: https://www.worthe-it.co.za/blog/2017-09-19-quick-introduction-to-graphviz.html#:~:text=Ranks%20and%20Subgraphs,placed%20further%20to%20the%20right.
-graph.add_subgraph(animals, rank="same", name="main")
 top = [b['34'].title, b['34'].told_by, b['34'].told_to]
+# Rank explained: https://www.worthe-it.co.za/blog/2017-09-19-quick-introduction-to-graphviz.html#:~:text=Ranks%20and%20Subgraphs,placed%20further%20to%20the%20right.
 graph.add_subgraph(top, rank="same", name="outer")
+graph.add_subgraph(animals, rank="same", name="main")
 graph.unflatten("-f -l3 -c 5").layout()
 graph.write("book-1.dot")
 graph.draw("book-1.pdf")
