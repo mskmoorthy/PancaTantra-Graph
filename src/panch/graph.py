7ca90da0115effde109f60e458f5f944680a80d0
T.V Raman <tv.raman.tv@gmail.com>
2022-03-26 19:15:27 -0700

    cleanup

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -10,22 +10,17 @@ b = entities.book_1
 c = entities.cast_1
 
 graph = pgv.AGraph(directed=True, name="book-1", label="graphatantra")
-[
-    graph.add_node(c[i].name, color=c[i].color, style="filled", fontsize="8pt")
-    for i in c
-]  #cast_1
+c_props = {"style": "filled", "fontsize": "8pt"}
+[graph.add_node(c[i].name, color=c[i].color, **c_props) for i in c]  #cast_1
 for i in b:
     graph.add_node(b[i].title, shape="box", fontsize="10pt")
     graph.add_node(b[i].moral, shape="rectangle", fontsize="7pt")
     ec = c[b[i].told_by].color
     graph.add_edge(b[i].told_by, b[i].title, color=ec)
     graph.add_edge(b[i].title, b[i].told_to, color=ec)
-    graph.add_edge(b[i].title, b[i].moral, arrowhead="halfopen")
+    graph.add_edge(b[i].title, b[i].moral, color=ec, carrowhead="halfopen")
     if b[i].stories is not None:
-        [
-            graph.add_edge(b[i].title, b[j].title, arrowhead="diamond")
-            for j in b[i].stories
-        ]  # contained stories
+        [graph.add_edge(b[i].title, b[j].title) for j in b[i].stories]
 
 animals = ['rusty', 'lively', 'crafty', 'cautious']
 top = [b['34'].title, b['34'].told_by, b['34'].told_to]
