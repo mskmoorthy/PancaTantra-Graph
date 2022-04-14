a0d7a0aa6508b444f647f2574b3e5806be219042
T.V Raman <tv.raman.tv@gmail.com>
2022-04-14 10:02:23 -0700

    gradient fill for main story

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -41,7 +41,7 @@ animals = ['rusty', 'lively', 'crafty', 'cautious']
 top = [b['0'].title, b['0'].told_by, b['0'].told_to, b['0'].moral]
 # Rank explained:
 # https://www.worthe-it.co.za/blog/2017-09-19-quick-introduction-to-graphviz.html#:~:text=Ranks%20and%20Subgraphs,placed%20further%20to%20the%20right.
-graph.add_subgraph(top, rank="same", name="outer")
+graph.add_subgraph(top, rank="same", name="outer", bgcolor="gray:")
 graph.add_subgraph(animals, rank="source", name="main")
 
 
