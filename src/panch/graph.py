e1b4fa5d29c819d4ac5622cb5a07c61d1203ffff
T.V Raman <tv.raman.tv@gmail.com>
2022-04-15 07:16:18 -0700

    build book1a

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -7,59 +7,63 @@ import entities
 b = entities.book_1
 c = entities.cast_1
 
-graph = pgv.AGraph(directed=True,
-                   name="book-1",
-                   label="graphatantra",
-                   fontname="helvetica")
-c_props = {"style": "filled, bold", "fontsize": "6pt"}
-t_props = {"shape": "box", "fontsize": "6pt", "style": "filled"}
-i_props = {
-    "style": "dashed,radial",
-    "rank": "same",
-    "bgcolor": "CornflowerBlue:PaleGoldenrod"
-}
-m_props = {
-    "fontsize": "6pt",
-    "style": "dotted",
-    "shape": "rectangle",
-    "fontname": "helvetica italic"
-}
-tm_props = {"carrowhead": "filled"}
-[graph.add_node(c[i].name, color=c[i].color, **c_props) for i in c]
-for i in sorted(b, key=int):
-    ec = c[b[i].told_by].color
-    s_label = "{}: {}".format(i, b[i].title)
-    graph.add_node(b[i].title, label=s_label, color=ec, **t_props)
-    graph.add_node(b[i].moral, color=ec, **m_props)
-    graph.add_edge(b[i].told_by, b[i].title, color=ec)
-    graph.add_edge(b[i].title, b[i].told_to, color=ec)
-    graph.add_edge(b[i].title, b[i].moral, color=ec, **tm_props)
-    if b[i].stories is not None:
-        [
-            graph.add_edge(b[i].title, b[j].title, style="dotted")
-            for j in b[i].stories
-        ]
 
-inner = [b[i].stories for i in b if i != '0' and b[i].stories is not None]
+def graph_a():
+    "Version A. Morals are nodes."
+    graph = pgv.AGraph(directed=True,
+                       name="book-1a",
+                       label="graphatantra",
+                       fontname="helvetica")
+    c_props = {"style": "filled, bold", "fontsize": "6pt"}
+    t_props = {"shape": "box", "fontsize": "6pt", "style": "filled"}
+    i_props = {
+        "style": "dashed,radial",
+        "rank": "same",
+        "bgcolor": "CornflowerBlue:PaleGoldenrod"
+    }
+    m_props = {
+        "fontsize": "6pt",
+        "style": "dotted",
+        "shape": "rectangle",
+        "fontname": "helvetica italic"
+    }
+    tm_props = {"carrowhead": "filled"}
+    [graph.add_node(c[i].name, color=c[i].color, **c_props) for i in c]
+    for i in sorted(b, key=int):
+        ec = c[b[i].told_by].color
+        s_label = "{}: {}".format(i, b[i].title)
+        graph.add_node(b[i].title, label=s_label, color=ec, **t_props)
+        graph.add_node(b[i].moral, color=ec, **m_props)
+        graph.add_edge(b[i].told_by, b[i].title, color=ec)
+        graph.add_edge(b[i].title, b[i].told_to, color=ec)
+        graph.add_edge(b[i].title, b[i].moral, color=ec, **tm_props)
+        if b[i].stories is not None:
+            [
+                graph.add_edge(b[i].title, b[j].title, style="dotted")
+                for j in b[i].stories
+            ]
 
-animals = ['rusty', 'lively', 'crafty', 'cautious']
-top = [b['0'].title, b['0'].told_by, b['0'].told_to, b['0'].moral]
-# Rank explained:
-# https://www.worthe-it.co.za/blog/2017-09-19-quick-introduction-to-graphviz.html#:~:text=Ranks%20and%20Subgraphs,placed%20further%20to%20the%20right.
-graph.add_subgraph(top,
-                   rank="same",
-                   name="outer",
-                   bgcolor="LightGray:PaleTurquoise")
-graph.add_subgraph(animals, rank="source", name="main")
-for i in range(len(inner)):
-    subtitles = [b[j].title for j in inner[i]]
-    graph.add_subgraph(subtitles, name="inner_{}".format(i), **i_props)
+    inner = [b[i].stories for i in b if i != '0' and b[i].stories is not None]
+
+    animals = ['rusty', 'lively', 'crafty', 'cautious']
+    top = [b['0'].title, b['0'].told_by, b['0'].told_to, b['0'].moral]
+    # Rank explained:
+    # https://www.worthe-it.co.za/blog/2017-09-19-quick-introduction-to-graphviz.html#:~:text=Ranks%20and%20Subgraphs,placed%20further%20to%20the%20right.
+    graph.add_subgraph(top,
+                       rank="same",
+                       name="outer",
+                       bgcolor="LightGray:PaleTurquoise")
+    graph.add_subgraph(animals, rank="source", name="main")
+    for i in range(len(inner)):
+        subtitles = [b[j].title for j in inner[i]]
+        graph.add_subgraph(subtitles, name="inner_{}".format(i), **i_props)
+    graph.unflatten("-f -l3").layout()
+    graph.write("book-1a.dot")
 
 
 def main():
-    "DDraw the graph"
-    graph.unflatten("-f -l3").layout()
-    graph.write("book-1.dot")
+    "DDraw the graphs"
+    graph_a()
 
 
 if __name__ == '__main__':
