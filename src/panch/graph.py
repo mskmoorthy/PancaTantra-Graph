4d42c252b4f6dd89977425be18ea1932a685aebf
T.V Raman <tv.raman.tv@gmail.com>
2022-04-17 07:13:00 -0700

    cleanup

--- src/panch/graph.py
+++ src/panch/graph.py
@@ -64,7 +64,6 @@ def graph_a():
                        bgcolor="LightBlue:LightGray")
     for i in range(len(inner)):
         subtitles = [b[j].title for j in inner[i]]
-        # inner_to_by will have duplicates
         inner_by_to = [b[j].told_by for j in inner[i]]
         inner_by_to += [b[j].told_to for j in inner[i]]
         graph.add_subgraph(subtitles + inner_by_to,
