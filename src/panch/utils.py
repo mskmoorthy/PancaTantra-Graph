d72faa47c560679aea6986105a40e7d2e78316df
T.V Raman <tv.raman.tv@gmail.com>
2022-05-17 15:26:15 -0700

    cleanup utils to handle book 1 band book 2

--- src/panch/utils.py
+++ src/panch/utils.py
@@ -2,66 +2,49 @@
 __author__ = "tv.raman.tv@gmail.com"
 
 import entities
-import graph
 
 
-def cast_extra():
+def cast_extra(b, c):
     "identify redundant cast members"
-     s1 = entities.book_1
-    s2 = entities.book_2
-    c1 = [s1[id].told_to for id in s2] + [s1[id].told_by for id in s1]
-    c2 = [s2[id].told_to for id in s2] + [s2[id].told_by for id in s2]
-    all = [s2[i].told_by for i in s2] + [s2[i].told_to for i in s2]
-    return {i for i in all if i not in c2}
+    all = [b[i].told_by for i in b] + [b[i].told_to for i in b]
+    return {i for i in all if i not in c}
 
 
-def cast_missing():
-    #s1 = entities.book_1
-    s2 = entities.book_2
-
-    c2 = [s2[id].told_to for id in s2] + [s2[id].told_by for id in s2]
-    return {w for w in c2 if w not in entities.cast_2}
+def cast_missing(b, c):
+    who = [b[id].told_to for id in b] + [b[id].told_by for id in b]
+    return {w for w in who if w not in c}
 
 
-def stories_missing():
-    [i for i in entities.book_2['0'].stories if i not in entities.book_2]
+def stories_missing(b):
+    [i for i in b['0'].stories if i not in b]
 
 
-def report(g):
-    "Show interesting facts from our graph."
-    b2 = entities.book_2
-    c2 = entities.cast_2
-    print("Cast: Degrees")
-    c_iter = g.degree_iter([n for n in c2])
-    [print(i) for i in c_iter]
-    b_iter = g.degree_iter([b2[n].title for n in b2])
-    print("Book: Degrees")
-    [print(i) for i in b_iter]
-
-
-def moral_len():
+def moral_len(name, b):
     "Show length of morals."
-    print("Moral Lengths")
-    m = [entities.book_1[i].moral for i in entities.book_1]
-    [print(len(i), ": ", i) for i in sorted(m, key=len)]
-    print("Moral Lengths")
-    m = [entities.book_2[i].moral for i in entities.book_2]
+    print("Moral Lengths In {}".format(name))
+    m = [b[i].moral for i in b]
     [print(len(i), ": ", i) for i in sorted(m, key=len)]
 
 
-def title_len():
+def title_len(name, b):
     "Show length of titles."
-    print("Title Lengths")
-    m = [entities.book_1[i].title for i in entities.book_1]
-    [print(len(i), ": ", i) for i in sorted(m, key=len)]
-    print("Title Lengths")
-    m = [entities.book_2[i].title for i in entities.book_2]
+    print("Title Lengths In {}".format(name))
+    m = [b[i].title for i in b]
     [print(len(i), ": ", i) for i in sorted(m, key=len)]
 
 
 if __name__ == '__main__':
-    print("Missing Cast Members", cast_missing())
-    print("Missing stories", stories_missing())
-    print("Extra Cast Members", cast_extra())
-    moral_len()
-    title_len()
+    print("book-1 Missing Cast Members",
+          cast_missing(entities.book_1, entities.cast_1))
+    print("book-2 Missing Cast Members",
+          cast_missing(entities.book_2, entities.cast_2))
+    print("book-1: Missing stories", stories_missing(entities.book_1))
+    print("book-2: Missing stories", stories_missing(entities.book_2))
+    print("Book-1: Extra Cast Members",
+          cast_extra(entities.book_1, entities.cast_1))
+    print("Book-2: Extra Cast Members",
+          cast_extra(entities.book_2, entities.cast_2))
+    moral_len("book-1", entities.book_1)
+    title_len('book-1', entities.book_1)
+    moral_len("book-2", entities.book_2)
+    title_len('book-2', entities.book_2)
